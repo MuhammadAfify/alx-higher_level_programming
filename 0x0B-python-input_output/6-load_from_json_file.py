69990dd888f231475afdd5bc287f5d46a7ac82f6
MuhammadAfify <muhammadafefy@gmail.com>
2024-02-06 12:41:33 -0800

    python input output

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""function that creates an Object from JSON file"""
+
+import json
+
+def load_from_json_file(filename):
+    
+    """creates an Object from a “JSON file”"""
+    
+    with open(filename) as f:
+        json.load(f)
