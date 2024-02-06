f41232ad703636cf3ad2f18973bc66394011cf79
MuhammadAfify <muhammadafefy@gmail.com>
2024-02-06 12:34:18 -0800

    python input output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""returns an object represented by a JSON string"""
+
+import json
+
+def from_json_string(my_str):
+    
+    """returns Python data structure"""
+    
+    return json.loads(my_str)
