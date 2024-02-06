8b1d6792108222ca0dcc26f455eed7c431efb0dd
MuhammadAfify <muhammadafefy@gmail.com>
2024-02-06 12:17:48 -0800

    python input output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
+"""appends a string at the end of a text file"""
+
 def append_write(filename="", text=""):
+    
+    """appent string to the end
+    
+    Args:
+        filename (str): file name
+        text (str): string
+        
+    Return:
+        no. chars
+    
+    """
+    
     with open(filename, "a", encoding="utf-8") as f:
         f.write(text)
     return len(text)
