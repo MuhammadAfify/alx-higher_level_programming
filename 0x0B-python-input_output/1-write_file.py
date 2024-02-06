54155e3e2833394d3d34486df6a2dbc26c7109ad
MuhammadAfify <muhammadafefy@gmail.com>
2024-02-06 12:11:06 -0800

    python input output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""writes a string to a text file"""
+
+def write_file(filename="", text=""):
+    """write text to file"""
+    with open(filename,"w", encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
