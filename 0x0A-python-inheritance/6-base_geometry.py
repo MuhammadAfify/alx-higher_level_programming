8a7ff22089054b0e50fcd8a23e2d3795356c01b6
MuhammadAfify <muhammadafefy@gmail.com>
2024-02-07 14:07:56 -0800

    python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""class definition"""
+
+class BaseGeometry:
+    
+    """class BaseGeometry"""
+    
+    def area(self):
+        
+        """area method wit exception"""
+        
+        raise Exception("area() is not implemented")
