5daa06ce81b43c9c355f4252487a44968521fc7e
MuhammadAfify <muhammadafefy@gmail.com>
2024-04-14 05:40:03 -0700

    Python wit database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# script that lists all states from the database hbtn_0e_0_usa
+
+import sys
+import MYSQLdb
+
+if __name__ == "__main__":
+    conn = MYSQLdb.connect (user = sys.argv[1], password = sys.argv[2], db = sys.argv[3])
+    curs = conn.cursor()
+    curs.excute("SELECT * FROM `states`")
+    print(state) for state in curs.fetchall()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+Databases and Python! it's a project that aims to link database with python
