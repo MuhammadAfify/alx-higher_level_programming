187b2ec6f96a2c9b9dd3ee23b7eaead4f2c4879e
MuhammadAfify <muhammadafefy@gmail.com>
2024-04-30 10:47:37 -0700

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
 
-print("- Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
-print("\t- utf8 content: {}".format(html_str))
+    print("- Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+
